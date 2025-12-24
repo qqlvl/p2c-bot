d89ab0ec0b944bea4831693142651f25489b592f
qqlvl <art.lev.dev@gmail.com>
2025-12-24 19:51:42 +0300

    v.03

--- app/core/config.py
+++ app/core/config.py
@@ -2,17 +2,18 @@
 
 from functools import lru_cache
 
-from pydantic import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
     BOT_TOKEN: str
     DB_URL: str = "sqlite+aiosqlite:///./p2c.db"
 
-    class Config:
-        env_file = ".env"
-        env_file_encoding = "utf-8"
-        case_sensitive = False
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_file_encoding="utf-8",
+        case_sensitive=False,
+    )
 
 
 @lru_cache
